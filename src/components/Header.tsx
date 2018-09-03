457573d9a185c2e3ddeb00db72b4e791cb83643a
LindaBot <voidnullbot@outlook.com>
2018-09-03 15:50:41 +1200

    Module 5 complete

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-//import { Nav, Navbar, NavItem } from 'react-bootstrap';
-//import { IndexLinkContainer } from "react-router-bootstrap";
-import { Link } from 'react-router-dom';
 import {AppBar, IconButton, Toolbar, Typography} from '@material-ui/core/';
 import MenuIcon from '@material-ui/icons/Menu';
+import * as React from 'react';
+// import { Nav,  NavItem } from 'react-bootstrap';
+// import { IndexLinkContainer } from "react-router-bootstrap";
+import { Link } from 'react-router-dom';
 
 export const Header: React.StatelessComponent<{}> = () => {
     return (
