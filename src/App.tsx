aa2b3f851ba6b14c1c18927a7c4adebc85fd5721
LindaBot <voidnullbot@outlook.com>
2018-09-03 13:23:50 +1200

    Dropzone structure Implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import * as React from 'react';
+import Dropzone from 'react-dropzone'
+import Loader from 'react-loader-spinner'
 import './App.css';
 
-export default class App extends React.Component<{}> {
+
+interface IState {
+  imageFiles: any[],
+  results: any,
+  dropzone: any
+}
+
+export default class App extends React.Component<{}, IState>{
+
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      imageFiles: [],
+      results: "",
+      dropzone: this.onDrop.bind(this)
+    }
+  }
+
   public render() {
     return (
       <div className="container-fluid">
@@ -12,4 +31,43 @@ export default class App extends React.Component<{}> {
     </div>
     );
   }
+
+  public upload(base64String: string) {
+    fetch('https://danktrigger.azurewebsites.net/api/dank', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+      body: JSON.stringify({
+        file: base64String,
+      })
+    })
+    .then((response : any) => {
+      if (!response.ok) {
+        this.setState({results: response.statusText})
+      }
+      else {
+        response.json().then((data:any) => this.setState({results: data[0].class}))
+      }
+      return response
+    })
+  }
+
+  public onDrop(files: any) {
+    this.setState({
+      imageFiles: files,
+      results: ""
+    })
+    const file = files[0]
+    const reader = new FileReader();
+    reader.onload = (readerEvt) => {
+        const binaryString = readerEvt.target!!.result;
+        this.upload(btoa(binaryString))
+    };
+
+    reader.readAsBinaryString(file);
+  }
+
+  
+  
 }
